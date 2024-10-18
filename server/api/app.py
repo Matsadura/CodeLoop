02d0a1cd711b9d4ef62d2e0fe578d52ec1940e91
Matsadura <zidanditoo1@gmail.com>
2024-10-18 22:18:41 +0100

    [ADD] login & register routes

--- server/api/app.py
+++ server/api/app.py
@@ -2,6 +2,8 @@
 """ Starts the Flask web app """
 import os
 from api.views import app_views
+from api.views.auth import jwt
+from datetime import timedelta
 from dotenv import load_dotenv
 from flask import Flask, jsonify
 from flask_cors import CORS
@@ -9,9 +11,12 @@ from flask_cors import CORS
 
 load_dotenv()
 app = Flask(__name__)
+jwt.init_app(app)
 CORS(app, supports_credentials=True, resources={r"*": {"origins": "*"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
 HOST = "0.0.0.0"
 PORT = 5000
 
@@ -20,7 +25,7 @@ PORT = 5000
 @app.route('/volumes')
 def volume():
     """ A dummy route to test volumes of docker"""
-    return "Testing volumes: -<change me>-"
+    return "Testing volumes: -<change me >-"
 
 
 @app.errorhandler(404)
