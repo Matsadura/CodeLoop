bb01331d60a0f093a97bae5861ac208287970e54
Ali <104344010+ila36IX@users.noreply.github.com>
2024-10-22 17:32:09 +0000

    [ADD] Setting up Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import SignIn from './components/SignIn';
+import LogIn from './scenes/LogIn';
+import Register from './scenes/Register';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className='h-full w-full'>
-      <SignIn />
-    </div>
+    // <div className='h-full w-full'>
+    //   <Register />
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<NavBar />} />
+      </Routes>
+    </Router>
   )
 }
 
