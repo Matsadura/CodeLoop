7914ca25dc8144988cac7da31b521b0136a079bf
Matsadura <zidanditoo1@gmail.com>
2024-10-28 11:34:35 +0100

    [ADD] Code execution

--- server/api/views/submissions.py
+++ server/api/views/submissions.py
@@ -33,6 +33,92 @@ from models.submission import Submission
 from models.task import Task
 
 
+def test_submission(submission, code):
+    import time
+    import requests
+    from models.solution_result import Solution_Result
+    from models.task_test_cases import Task_Test_Cases
+    """Test the submission using Judge0."""
+    language = {'python': 71}
+    task_test_cases = storage.all_list_specific(
+        Task_Test_Cases, 'task_id', submission.task_id)
+
+    test_cases = [{
+        "test_case_id": tc.id,
+        "stdin": tc.input,
+        "expected_output": tc.expected_output
+    } for tc in task_test_cases]
+
+    submission_result = {}
+    for test in test_cases:
+        json_data = {
+            'source_code': code,
+            'language_id': language[submission.language],
+            'stdin': test['stdin'],
+            'expected_output': test['expected_output']
+        }
+
+        response = requests.post('http://localhost:2358/submissions',
+                                 json=json_data)
+
+        if response.status_code != 201:
+            print("Submission failed:", response.json())
+            return
+
+        token = response.json().get('token')
+        print(f"Submission token: {token}")
+
+        while True:
+            result_url = f'http://localhost:2358/submissions/{token}'
+
+            result_response = requests.get(result_url)
+
+            if result_response.status_code != 200:
+                print("Error fetching r esult:", result_response.json())
+                return
+
+            result_data = result_response.json()
+
+            if result_data['status']['id'] in [1, 2]:
+                print("Submission is still processing...")
+                time.sleep(1)
+            else:
+                if result_data['status']['id'] == 3:
+                    status = 'pass'
+                elif result_data['status']['id'] == 4:
+                    status = 'fail'
+                else:
+                    status = 'error'
+                # tmp print(result_data)
+                submission_result[test['test_case_id']] = {
+                    'status': status,
+                    'stdout': result_data['stdout'],
+                    'time': result_data['time'],
+                    'stderr': result_data['stderr'],
+                }
+                solution_result_data = {
+                    'submission_id': submission.id,
+                    'task_test_case_id': test['test_case_id'],
+                    'status': status,
+                    'stdout': result_data['stdout'],
+                    'stderr': result_data['stderr'],
+                    'time': result_data['time'],
+                }
+                solution_result = Solution_Result(**solution_result_data)
+                storage.new(solution_result)
+                storage.save()
+                print(submission_result)
+                break
+
+        for value in submission_result.values():
+            if value['status'] == 'fail':
+                submission.status = 'incorrect'
+                break
+            else:
+                submission.status = 'correct'
+            storage.save()
+
+
 @app_views.route('/tasks/<task_id>/submit', methods=['POST'])
 @jwt_required()
 def submit_task(task_id):
@@ -65,6 +151,8 @@ def submit_task(task_id):
         status='pending'
     )
     storage.new(submission)
+    storage.save()
+    test_submission(submission, data['code'])
     storage.save()
     return jsonify(submission.to_dict()), 201
 
